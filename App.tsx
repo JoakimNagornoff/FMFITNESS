b93fc39c262fcf1e848152f4c0d6a6f3d6480257
joakimnagornoff <joakim.nagornoff@live.se>
2024-03-01 13:40:55 +0100

    feat: added default styling to stack navigator

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/Home/HomeScreen';
@@ -10,8 +10,20 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#f3f3f3',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        <Stack.Group>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Group>
       </Stack.Navigator>
     </NavigationContainer>
   );
